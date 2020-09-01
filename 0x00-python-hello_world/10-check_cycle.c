3319ef40caf7a1a278f1487b1a581da4e1286a49
I7RANK <franfgh2000@hotmail.com>
2020-09-01 03:03:31 +0000

    fix 10 liebre y tortuga

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,8 +8,7 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp, *temp2;
-	int i;
+	listint_t *temp = NULL, *temp2 = NULL;
 
 	if (list == NULL || list->next == NULL)
 	{
@@ -19,13 +18,19 @@ int check_cycle(listint_t *list)
 	temp = list->next;
 	temp2 = list;
 
-	for (i = 0; temp; i++)
+	while (temp)
 	{
 		if (temp == temp2)
 		{
 			return (1);
 		}
-		temp = temp->next;
+
+		if (temp->next == NULL)
+		{
+			return (0);
+		}
+		temp2 = temp2->next;
+		temp = temp->next->next;
 	}
 	return (0);
 }
