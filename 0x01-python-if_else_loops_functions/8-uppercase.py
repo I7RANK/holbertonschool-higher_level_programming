6a4caebeecf0cb1ee4c59cc80d7958e50268964b
I7RANK <franfgh2000@hotmail.com>
2020-09-01 21:07:18 +0000

    fix 8 aonly 2 print()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    rest = 0
+
     for i in range(len(str)):
         if ord(str[i]) > 96 and ord(str[i]) < 123:
-            print("{:c}".format(ord(str[i]) - 32), end="")
-        else:
-            print("{:c}".format(ord(str[i])), end="")
+            rest = 32
+        print("{:c}".format(ord(str[i]) - rest), end="")
+        rest = 0
     print()
