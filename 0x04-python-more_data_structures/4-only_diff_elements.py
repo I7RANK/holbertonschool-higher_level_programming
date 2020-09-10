35b30a0c897188163ecb0b872e11533ca1946e2a
I7RANK <franfgh2000@hotmail.com>
2020-09-10 23:28:13 +0000

    uploading tasks 0 to 5

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+	only_one = []
+	for i in set_1:
+		if i not in set_2:
+			only_one.append(i)
+	for j in set_2:
+		if j not in set_1:
+			only_one.append(j)
+			
+	return only_one
