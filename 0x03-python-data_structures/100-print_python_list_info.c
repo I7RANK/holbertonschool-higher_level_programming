0c10b3137085b27cd5882ba1f2b7853522aa23f0
I7RANK <franfgh2000@hotmail.com>
2020-09-10 02:38:35 +0000

    no equals and if p not NULL

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,12 +12,17 @@ void print_python_list_info(PyObject *p)
 	int size_p = (int)PyList_Size(p), i;
 	char *str = NULL;
 
+	if (p == NULL)
+	{
+		return;
+	}
+
 	printf("[*] Size of the Python List = %d\n", size_p);
 	printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size_p; i++)
 	{
 		str = (char *)PyList_GET_ITEM(p, i)->ob_type->tp_name;
-		printf("Element %d = %s\n", i, str);
+		printf("Element %d: %s\n", i, str);
 	}
 }
