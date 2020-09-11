929908538a359f66350e3b71f2e86a6ceef403f1
I7RANK <franfgh2000@hotmail.com>
2020-09-11 03:02:12 +0000

    uploading task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    prev = 0
+    sum_roman = 0
+    conver_roman = {}
+    conver_roman['I'] = 1
+    conver_roman['V'] = 5
+    conver_roman['X'] = 10
+    conver_roman['L'] = 50
+    conver_roman['C'] = 100
+    conver_roman['D'] = 500
+    conver_roman['M'] = 1000
+    for i in roman_string:
+        if conver_roman[i] > prev:
+            sum_roman += conver_roman[i] - prev
+        else:
+            sum_roman += conver_roman[i]
+        prev = conver_roman[i]
+
+    return sum_roman
