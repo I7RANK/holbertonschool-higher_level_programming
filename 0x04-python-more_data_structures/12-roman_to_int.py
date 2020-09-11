1505093f7c45a827f2af9f657d67d2e1d9d0a208
I7RANK <franfgh2000@hotmail.com>
2020-09-11 03:32:34 +0000

    fix 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,10 @@ def roman_to_int(roman_string):
     prev = 0
     sum_roman = 0
     conver_roman = {}
+
+    if not roman_string:
+        return 0
+
     conver_roman['I'] = 1
     conver_roman['V'] = 5
     conver_roman['X'] = 10
@@ -10,9 +14,11 @@ def roman_to_int(roman_string):
     conver_roman['C'] = 100
     conver_roman['D'] = 500
     conver_roman['M'] = 1000
+    prev = conver_roman[roman_string[0]]
+
     for i in roman_string:
         if conver_roman[i] > prev:
-            sum_roman += conver_roman[i] - prev
+            sum_roman += conver_roman[i] - (prev * 2)
         else:
             sum_roman += conver_roman[i]
         prev = conver_roman[i]
