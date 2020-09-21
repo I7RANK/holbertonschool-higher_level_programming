4c78c95b520c4a8cd44181edc109c9d7678c474f
I7RANK <franfgh2000@hotmail.com>
2020-09-21 22:11:01 +0000

    fix return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             real_len += 1
     except IndexError:
         print()
-        return i
+        return real_len
     print()
-    return i + 1
+    return real_len
