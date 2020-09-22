2d42eb6886b2f566f51accffec749f2451380898
I7RANK <franfgh2000@hotmail.com>
2020-09-22 21:35:13 +0000

    uploading task 0 and 1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" class square """
+class Square:
+    """ a class """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class square """
+class Square:
+    """ a class """
+    def __init__(self,  size):
+        """ constructor
+        Argumemts:
+        size{int} -- the size of square """
+        self._Square__size = size
