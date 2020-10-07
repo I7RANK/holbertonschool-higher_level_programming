1de32b31a2687d46e80b0ef5ba32942f1fd3f2fe
I7RANK <franfgh2000@hotmail.com>
2020-10-07 00:25:11 +0000

    fix 10 import

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,21 +2,7 @@
 """ this module contains the class BaseGeometry """
 
 
-class BaseGeometry():
-    """ Class BaseGeometry """
-    def area(self):
-        """ raise an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates the value
-        Arguments:
-            name{str} - is one name
-            value{int} - is one value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
