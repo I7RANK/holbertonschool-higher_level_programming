8138b228fb5038397cd8331ccbc6f7fd67e1d273
I7RANK <franfgh2000@hotmail.com>
2020-10-13 18:11:21 +0000

    try fix 10 __init__ with setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,28 +22,11 @@ class Rectangle(Base):
             y (int, optional): value of y. Defaults to 0.
             id (int, optional): id of this instance. Defaults to None.
         """
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if y < 0:
-            raise ValueError("y must be >= 0")
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     """ GETTERS AND SETTERS
     """
