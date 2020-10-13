e139b40190507aa5e6760c5d851e0eb3d76a069d
I7RANK <franfgh2000@hotmail.com>
2020-10-13 19:20:14 +0000

    try with not list_...

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,10 +31,9 @@ class Base():
         Returns:
             str: str in json format
         """
-        if list_dictionaries is None:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
