f8fe9360a9a93c43f6baa9c41f5087fc99128a3f
I7RANK <franfgh2000@hotmail.com>
2020-11-16 18:31:11 -0500

    fixing tasks 9, 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
 -- lists all records of the table second_table
 -- Don’t list rows without a name value
-SELECT score, name FROM second_table where name != 'NULL' AND name != "";
+SELECT score, name FROM second_table where name IS NOT NULL
+ORDER BY score DESC;
