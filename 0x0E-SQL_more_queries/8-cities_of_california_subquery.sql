2670d8d9dba4016f082b19cbcadf0bb38c93883b
I7RANK <franfgh2000@hotmail.com>
2020-11-17 17:07:28 -0500

    fixing 8 with subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- lists all the cities of California that can be found in th database hbtn_0d_usa
-SELECT cities.id, cities.name FROM cities where state_id = 1 ORDER BY cities.id;
+SELECT cities.id, cities.name FROM cities WHERE state_id = (
+    SELECT id FROM states WHERE name = "California"
+) ORDER BY cities.id;
