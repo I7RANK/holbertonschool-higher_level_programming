4cb4b8bc735346488229028318d79e02c45fe9aa
I7RANK <franfgh2000@hotmail.com>
2020-12-18 22:17:38 -0500

    uploading task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" finds a peak in a list of unsorted integers. """
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): unsorted list
+    """
+    if list_of_integers == []:
+        return
+
+    list_ = list_of_integers
+    l_len = len(list_)
+    peak = 100000
+    i = 0
+    while i < l_len:
+        """ print(list_[i], i, l_len) """
+        if i == 0:
+            if i < l_len - 1:
+                if list_[i] > list_[i + 1]:
+                    return list_[i]
+        else:
+            if i < l_len - 1:
+                if list_[i] > list_[i + 1]:
+                    if list_[i] > list_[i - 1]:
+                        return list_[i]
+                    i += 1
+
+        i += 1
+
+    return max(list_)
